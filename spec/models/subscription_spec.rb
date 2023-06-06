5765c7b5149b42c8977801a8e8f92f65aba13960
Bobby Luly <bobbyluly@gmail.com>
2023-06-06 16:55:17 -0600

    Create model tests

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  describe 'relationships' do
+    it { should belong_to :customer }
+    it { should belong_to :tea }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+    it { should validate_numericality_of :price }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :frequency }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:status).with_values([:active, :cancelled]) }
+    it { should define_enum_for(:frequency).with_values([:weekly, :monthly, :quarterly]) }
+  end
+end
---
+++ spec/models/tea_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Tea, type: :model do
+  describe 'relationships' do
+    it { should have_many :subscriptions }
+    it { should have_many(:customers).through(:subscriptions) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :temperature }
+    it { should validate_numericality_of(:temperature).is_greater_than_or_equal_to(40) }
+    it { should validate_numericality_of(:temperature).is_less_than_or_equal_to(180) }
+    it { should validate_presence_of :brew_time }
+    it { should validate_numericality_of(:brew_time).is_greater_than(0) }
+    it { should validate_presence_of :title }
+    it { should validate_uniqueness_of :title }
+    it { should validate_presence_of :description }
+  end
+end
