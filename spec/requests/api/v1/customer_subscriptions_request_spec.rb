5e1135ea25cc26661622c0b539ea110a256c3daf
Bobby Luly <bobbyluly@gmail.com>
2023-06-07 16:57:08 -0600

    Test: write happy and sad path tests

--- spec/requests/api/v1/customer_subscriptions_request_spec.rb
+++ spec/requests/api/v1/customer_subscriptions_request_spec.rb
@@ -32,9 +32,28 @@ RSpec.describe 'Customer Subscriptions API' do
         subscriptions = JSON.parse(response.body, symbolize_names: true)
 
         expect(subscriptions[:data].count).to eq(3)
+        expect(subscriptions[:data].sample.keys).to eq([:id, :type, :attributes])
+        expect(subscriptions[:data][0][:attributes].keys).to eq([:title, :price, :status, :frequency, :teas])
+        expect(subscriptions[:data][0][:id]).to eq(@subscription1.id.to_s)
+        expect(subscriptions[:data][0][:type]).to eq('subscription')
         expect(subscriptions[:data][0][:attributes][:title]).to eq(@subscription1.title)
-        expect(subscriptions[:data][1][:attributes][:title]).to eq(@subscription2.title)
-        expect(subscriptions[:data][2][:attributes][:title]).to eq(@subscription3.title)
+        expect(subscriptions[:data][0][:attributes][:price]).to eq(@subscription1.price)
+        expect(subscriptions[:data][0][:attributes][:status]).to eq(@subscription1.status)
+        expect(subscriptions[:data][0][:attributes][:frequency]).to eq(@subscription1.frequency)
+        expect(subscriptions[:data][0][:attributes][:teas].count).to eq(2)
+        expect(subscriptions[:data][0][:attributes][:teas][0][:id]).to eq(@tea_1.id)
+        expect(subscriptions[:data][0][:attributes][:teas][0][:title]).to eq(@tea_1.title)
+      end
+    end
+
+    context 'when unsuccessful' do
+      it 'returns a 404 if customer does not exist' do
+        get api_v1_customer_subscriptions_path(999)
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(404)
+        expect(parsed[:errors]).to eq("Couldn't find Customer with 'id'=999")
       end
     end
   end
