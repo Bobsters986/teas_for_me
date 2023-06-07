abfc6e24f9d37f003e22f21842d902a6e16f0f58
Bobby Luly <bobbyluly@gmail.com>
2023-06-07 16:55:37 -0600

    Remove frequency

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :subscription do
     title { Faker::Fantasy::Tolkien.poem }
     price { Faker::Number.decimal(l_digits: 2) }
-    frequency { 1 }
     association :customer
   end
 end
