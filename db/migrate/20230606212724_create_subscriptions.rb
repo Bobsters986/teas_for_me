89e635dfedbb10416e678e464b1f727eaf25af3f
Bobby Luly <bobbyluly@gmail.com>
2023-06-07 11:21:29 -0600

    Add default 1 to frequency. Change tea/subscription relationship to many to many

--- db/migrate/20230606212724_create_subscriptions.rb
+++ db/migrate/20230606212724_create_subscriptions.rb
@@ -2,11 +2,10 @@ class CreateSubscriptions < ActiveRecord::Migration[7.0]
   def change
     create_table :subscriptions do |t|
       t.references :customer, null: false, foreign_key: true
-      t.references :tea, null: false, foreign_key: true
       t.string :title
       t.float :price
       t.integer :status, default: 0
-      t.integer :frequency
+      t.integer :frequency, default: 1
 
       t.timestamps
     end
