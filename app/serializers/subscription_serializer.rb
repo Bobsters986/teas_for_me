e390b8568c8aa65b7d389ed685f06027e850adf5
Bobby Luly <bobbyluly@gmail.com>
2023-06-08 16:45:14 -0600

    Customize teas attribute to not return timestamps in response

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,4 +1,16 @@
 class SubscriptionSerializer
   include JSONAPI::Serializer
   attributes :title, :price, :status, :frequency, :teas
+
+  attribute :teas do |subscription|
+    subscription.teas.map do |tea|
+      {
+        id: tea.id,
+        title: tea.title,
+        description: tea.description,
+        temperature: tea.temperature,
+        brew_time: tea.brew_time
+      }
+    end
+  end
 end
