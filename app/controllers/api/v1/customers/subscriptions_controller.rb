4efee3a50d30597251a56a8a992a187ad7e4302d
Bobby Luly <bobbyluly@gmail.com>
2023-06-07 22:12:48 -0600

    Feat: add create action logic, make create_subscription_teas private method

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -2,6 +2,33 @@ class Api::V1::Customers::SubscriptionsController < ApplicationController
   def index
     customer = Customer.find(params[:customer_id])
     render json: SubscriptionSerializer.new(customer.subscriptions)
+  end
 
+  def create
+    customer = Customer.find(params[:customer_id])
+    subscription = customer.subscriptions.new(subscription_params)
+
+    if subscription.save
+      create_subscription_teas(subscription)
+      render json: SubscriptionSerializer.new(subscription), status: 201
+    else
+      render json: { errors: subscription.errors.full_messages.join(', ') }, status: 400
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:title, :price, :status, :frequency)
+  end
+
+  def create_subscription_teas(subscription)
+    params[:teas].each do |tea_id|
+      tea = Tea.find(tea_id)
+
+      if !tea.nil?
+        SubscriptionTea.create!(tea: tea, subscription: subscription)
+      end
+    end
   end
 end
