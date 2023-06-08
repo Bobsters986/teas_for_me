6db85786ec7a1df3bdc960b8e5c182083279aa16
Bobby Luly <bobbyluly@gmail.com>
2023-06-07 22:10:33 -0600

    Test: create happy and sad path tests for subscribing a customer to a new subscription

--- spec/requests/api/v1/customer_subscriptions_request_spec.rb
+++ spec/requests/api/v1/customer_subscriptions_request_spec.rb
@@ -20,6 +20,8 @@ RSpec.describe 'Customer Subscriptions API' do
 
     @subscription_tea5 = create(:subscription_tea, subscription: @subscription3, tea: @tea_1)
     @subscription_tea6 = create(:subscription_tea, subscription: @subscription3, tea: @tea_3)
+
+    @headers = { 'CONTENT_TYPE' => 'application/json' }
   end
 
   describe 'Customer Subscriptions Index' do
@@ -54,6 +56,92 @@ RSpec.describe 'Customer Subscriptions API' do
 
         expect(response.status).to eq(404)
         expect(parsed[:errors]).to eq("Couldn't find Customer with 'id'=999")
+      end
+    end
+  end
+
+  describe "Customer Subscriptions Create" do
+    context "when successful" do
+      it "creates a new subscription for a customer" do
+        expect(@customer.subscriptions.count).to eq(3)
+
+        subscription_params = {
+          title: "Tea-licious Subscription",
+          price: 10.00,
+          status: "active",
+          frequency: "monthly",
+          teas: [@tea_1.id, @tea_2.id]
+        }
+
+        post api_v1_customer_subscriptions_path(@customer.id), headers: @headers, params: subscription_params, as: :json
+
+        expect(response).to be_successful
+
+        subscription = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(subscription[:data][:id]).to eq(Subscription.last.id.to_s)
+        expect(@customer.subscriptions.count).to eq(4)
+        expect(subscription[:data][:type]).to eq('subscription')
+        expect(subscription[:data][:attributes][:title]).to eq(subscription_params[:title])
+        expect(subscription[:data][:attributes][:price]).to eq(subscription_params[:price])
+        expect(subscription[:data][:attributes][:status]).to eq(subscription_params[:status])
+        expect(subscription[:data][:attributes][:frequency]).to eq(subscription_params[:frequency])
+        expect(subscription[:data][:attributes][:teas].count).to eq(2)
+        expect(subscription[:data][:attributes][:teas][0][:id]).to eq(@tea_1.id)
+        expect(subscription[:data][:attributes][:teas][0][:title]).to eq(@tea_1.title)
+      end
+    end
+
+    context "when unsuccessful" do
+      it "returns a 404 if customer does not exist" do
+        subscription_params = {
+          title: "Tea-licious Subscription",
+          price: 10.00,
+          status: 1,
+          frequency: 0,
+          teas: [@tea_1.id, @tea_2.id]
+        }
+
+        post api_v1_customer_subscriptions_path(999), headers: @headers, params: subscription_params, as: :json
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(404)
+        expect(parsed[:errors]).to eq("Couldn't find Customer with 'id'=999")
+      end
+
+      it "returns a 400 error if subscription is not created because of invalid params" do
+        subscription_params = {
+          title: "",
+          price: "free",
+          status: "active",
+          frequency: "monthly",
+          teas: [@tea_1.id, @tea_2.id]
+        }
+
+        post api_v1_customer_subscriptions_path(@customer.id), headers: @headers, params: subscription_params, as: :json
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(400)
+        expect(parsed[:errors]).to eq("Title can't be blank, Price is not a number")
+      end
+
+      it "returns a 404 error if subscription is not created because of invalid tea ids" do
+        subscription_params = {
+          title: "Tea-licious Subscription",
+          price: 10.00,
+          status: "active",
+          frequency: "monthly",
+          teas: [@tea_1.id, 888]
+        }
+
+        post api_v1_customer_subscriptions_path(@customer.id), headers: @headers, params: subscription_params, as: :json
+
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(404)
+        expect(parsed[:errors]).to eq("Couldn't find Tea with 'id'=888")
       end
     end
   end
